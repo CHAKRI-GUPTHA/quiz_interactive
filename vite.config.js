7efc9772507d9fd050b03510c127d91e45a8896a
CHAKRI-GUPTHA <chakriguptha123@gmail.com>
2026-02-17 18:42:29 +0530

    Configured for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  base: '/quiz_interactive/',
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-});
+  // Use GitHub Pages base only for production build.
+  base: command === 'build' ? '/quiz_interactive/' : '/',
+}))
